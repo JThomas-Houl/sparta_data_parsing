18e8b706d918c668b942522bd9aea23f8788787a
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-04-10 15:26:50 +0100

    testing muilty dimention arrays

--- arrays/rspec_array_data_lab/spec/array_spec.rb
+++ arrays/rspec_array_data_lab/spec/array_spec.rb
@@ -5,16 +5,19 @@ describe 'working through various arrays' do
   context 'standard 1 dimensional arrays' do
     dog_breeds = ['jack russell', 'boxer', 'bloodhound', 'greyhound']
 
-    it 'the first entry should equal boxer' do
+    it 'the first entry should equal jack russell' do
       # Use array methods only
+      expect(dog_breeds.first).to eq 'jack russell'
     end
 
     it 'the last entry should equal greyhound' do
       # Use array methods only
+      expect(dog_breeds.last).to eq 'greyhound'
     end
 
     it 'the second entry should equal boxer' do
       # Use array index only i.e. array[0]
+      expect(dog_breeds[1]).to eq 'boxer'
     end
 
   end
@@ -25,14 +28,20 @@ describe 'working through various arrays' do
 
     it 'the first dog name should equal archie' do
       # Use a mix of array methods and indexes
+      expect(dog_breed_and_names[0][1]).to eq 'archie'
+      
     end
 
     it 'the second last entry should be a bloodhound called droopy' do
       # Use array index only i.e. array[0]
+      expect(dog_breed_and_names[2][1]).to eq 'droopy'
+      
     end
 
     it 'the second entry should be a boxer' do
       # Use array index only i.e. array[0]
+      expect(dog_breed_and_names[1][0]).to eq 'boxer'
+      
     end
 
   end
@@ -42,14 +51,26 @@ describe 'working through various arrays' do
 
     it 'the last dog should be called streak and is an old dog' do
       # Use a mix of array methods and indexes
+      expect(dog_breed_names_age_and_health[3][1]).to eq 'streak'
+
+      expect(dog_breed_names_age_and_health[3][2][1]).to eq 'old dog'
+      
     end
 
     it 'the bloodhound should be 6 years old and healthy' do
       # Use a mix of array methods and indexes
+      expect(dog_breed_names_age_and_health[2][2][0]).to eq 6
+
+      expect(dog_breed_names_age_and_health[2][2][1]).to eq 'healthy'
+
+      
     end
 
     it 'the first dog should be a jack russell and 4 years old' do
       # Use array index only i.e. array[0]
+      expect(dog_breed_names_age_and_health[0][0]).to eq 'jack russell'
+      expect(dog_breed_names_age_and_health[0][2][0]).to eq 4
+      
     end
 
   end
